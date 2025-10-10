e4752fbed267bf8152e0d8ca9e5c7b9691a795ff
raystatic <rahul9650ray@gmail.com>
2025-10-10 19:30:39 +0530

    feat: determine project git root

--- src/main/kotlin/com/github/raystatic/commitgeniedevfesttemp/CommitMessageAction.kt
+++ src/main/kotlin/com/github/raystatic/commitgeniedevfesttemp/CommitMessageAction.kt
@@ -3,12 +3,21 @@ package com.github.raystatic.commitgeniedevfesttemp
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
 
 class CommitMessageAction : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project ?: return
-        Messages.showInfoMessage(project, "Initial setup", "CommitGenie")
+
+
+        // fetch git root
+        val gitRoot = ProjectLevelVcsManager.getInstance(project).allVcsRoots.firstOrNull()?.path?.path
+        if (gitRoot.isNullOrEmpty()) {
+            Messages.showErrorDialog(project, "Could not determine Git root.", "Commit Genie")
+            return
+        }
+
     }
 
 }
